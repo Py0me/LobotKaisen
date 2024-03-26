af4ee094edc721a01585130f1c73c41cc0bdf96b
RandomGuyWithoutY <randomguywithouty@protonmail.com>
2024-03-26 15:10:32 +0100

    bot: fix jjk_shitpost

--- LobotKaisen/bot_cmd/jjk_shitpost.py
+++ LobotKaisen/bot_cmd/jjk_shitpost.py
@@ -10,13 +10,13 @@ from interactions import (
 import interactions
 
 class JJKShitpostExt(Extension):
-	jjk_shitpost = SlashCommand(name='jjk-shitpost', description='Includes all of the Lobotomy essentials!', scopes=[931930956284178512])
+	jjk_shitpost = SlashCommand(name='jjk-shitpost', description='Includes all of the Lobotomy essentials!')
 
-	@jjk_shitpost.subcommand(sub_cmd_name='fill-quote-a', sub_cmd_description='Are you X because you are Y, or are you Y because you are X?')
+	@jjk_shitpost.subcommand(sub_cmd_name='are-you-because', sub_cmd_description='Are you X because you are Y, or are you Y because you are X?')
 	@slash_option(name='first-insert', argument_name='first', description='The first term to insert into the template', opt_type=OptionType.STRING, required=True)
 	@slash_option(name='second-insert', argument_name='second', description='The second term to insert into the template', opt_type=OptionType.STRING, required=True)
 	@slash_option(name='public', description='Wether to post the generated quote publicly or not', opt_type=OptionType.BOOLEAN, required=False)
-	async def fill_quote_a(self, ctx: SlashContext, first: str, second: str, public: bool):
+	async def are_you_because(self, ctx: SlashContext, first: str, second: str, public: bool):
 		await ctx.send('Are you %(first)s because you are %(second)s, or are you %(second)s because you are %(first)s?' % {'first': first, 'second': second}, ephemeral=not public if public is not None else True)
 
 
